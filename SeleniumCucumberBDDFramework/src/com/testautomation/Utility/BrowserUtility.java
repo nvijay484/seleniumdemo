323b6f9af9217f82ece0c7dd120231cdceacd4a2
nvijay484 <nvijay484@gmail.com>
2021-11-24 21:54:28 +0530

    Add files via upload

---
+++ SeleniumCucumberBDDFramework/src/com/testautomation/Utility/BrowserUtility.java
@@ -0,0 +1,52 @@
+package com.testautomation.Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserUtility
+{
+	public static WebDriver OpenBrowser(WebDriver driver,String browserName,String url) throws InterruptedException
+	{
+		if(browserName.equals("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+//			System.setProperty("webdriver.chrome.driver", "E:\\driver\\chromedriver.exe");
+			driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get(url);
+			Thread.sleep(5000);
+			return driver;	
+		}else 
+		if(browserName.equals("IE"))
+		{
+			WebDriverManager.iedriver().setup();
+//			System.setProperty("webdriver.ie.driver", "L:\\TestAutomationFramework\\CucumberJarFiles\\chromedriver_win32_2.37\\chromedriver.exe");
+			DesiredCapabilities capabilities=new DesiredCapabilities();
+			capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, "accept");
+			capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+			capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			capabilities.setCapability("ignoreZoomSetting", true);
+			capabilities.setCapability("requireWindowFocus", true);//to move mouse manually			
+			driver=new InternetExplorerDriver();
+			driver.manage().window().maximize();
+			driver.get(url);
+			return driver;	
+		} else if(browserName.equals("Firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+//			System.setProperty("webdriver.ie.driver", "L:\\TestAutomationFramework\\CucumberJarFiles\\chromedriver_win32_2.37\\chromedriver.exe");
+			driver=new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.get(url);
+			return driver;	
+		}
+			return null;			
+	}
+
+}
